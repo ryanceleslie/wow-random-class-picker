9a09ea3c7d35cc9fdd1fefe27531e7c7330207fe
Ryan Celeslie <ryan.celeslie@outlook.com>
2024-06-03 14:09:09 -0400

    Added tons of new icons, changed main JSON structure, added a better layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Random Class Picker!';
 
+  isShown = false;
+
   classes: any;
   url: string = '../assets/classes.json';
 
   randomClass: any;
   randomSpec: any;
+  randomHero: any;
   randomRace: any;
 
   getRandomSelection() {
     fetch(this.url).then(response => response.json())
       .then(results => {
 
+        this.isShown = true;
+
         this.classes = results;
 
         this.randomClass = this.classes[Math.floor(Math.random() * this.classes.length)];
 
         this.randomSpec = this.randomClass.specs[Math.floor(Math.random() * this.randomClass.specs.length)];
+
+        this.randomHero = this.randomSpec.hero[Math.floor(Math.random() * this.randomSpec.hero.length)];
+
         this.randomRace = this.randomClass.races[Math.floor(Math.random() * this.randomClass.races.length)];
 
       });
