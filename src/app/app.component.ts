c257b88eaadbbd931dc354289e52b424218d2b63
Ryan Celeslie <ryan.celeslie@outlook.com>
2024-06-03 00:13:42 -0400

    Big update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wow-random-class-picker';
+  title = 'Random Class Picker!';
+
+  classes: any;
+  url: string = '../assets/classes.json';
+
+  randomClass: any;
+  randomSpec: any;
+  randomRace: any;
+
+  getRandomSelection() {
+    fetch(this.url).then(response => response.json())
+      .then(results => {
+
+        this.classes = results;
+
+        this.randomClass = this.classes[Math.floor(Math.random() * this.classes.length)];
+
+        this.randomSpec = this.randomClass.specs[Math.floor(Math.random() * this.randomClass.specs.length)];
+        this.randomRace = this.randomClass.races[Math.floor(Math.random() * this.randomClass.races.length)];
+
+      });
+
+  }
 }
